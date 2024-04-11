d4e9ea142f089feed3d45ac134b523162eb5ee82
Ylenia <05ylenia@gmail.com>
2024-04-11 14:33:38 +0200

    <Script> entfernt

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-<script>
+
         var map = L.map('map').setView([43.35, 12.566667], 13);
-        
+
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -8,4 +8,3 @@
 
         var marker = L.marker([43.35, 12.566667]).addTo(map);
         marker.bindPopup("<b>Ciao bello!</b><br>Come and visit me.").openPopup();
-    </script>
