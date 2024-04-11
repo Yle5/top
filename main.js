e471d9efab4569dffc2d96200d4711b2ac8ade68
Ylenia <05ylenia@gmail.com>
2024-04-11 14:17:39 +0200

    Main.js hinzugefügt

---
+++ main.js
@@ -0,0 +1,11 @@
+<script>
+        var map = L.map('map').setView([43.35, 12.566667], 13);
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var marker = L.marker([43.35, 12.566667]).addTo(map);
+        marker.bindPopup("<b>Ciao bello!</b><br>Come and visit me.").openPopup();
+    </script>
